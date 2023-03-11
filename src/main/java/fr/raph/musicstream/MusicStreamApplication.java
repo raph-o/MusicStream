17b07a548fd96cdcc6cf8f31e1418fc71d56d711
raph <orsi.raph@gmail.com>
2023-03-11 10:15:26 +0100

    Initial commit

---
+++ src/main/java/fr/raph/musicstream/MusicStreamApplication.java
@@ -0,0 +1,13 @@
+package fr.raph.musicstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStreamApplication.class, args);
+    }
+
+}
